6bb39a9095b9cdcd1d1851dbe178d9d9b1504253
lipd <lipeidao@gmail.com>
2021-03-31 15:24:54 +0800

    处理 children 中的元素

--- src/tiny-react/createElement.js
+++ src/tiny-react/createElement.js
@@ -1,7 +1,18 @@
 export default function createElement(type, props, ...children) {
+  const childElements = [...children].reduce((accu, child) => {
+    if (child === false || child === true || child === null) {
+      return accu
+    } else if (child instanceof Object) {
+      accu.push(child)
+    } else {
+      accu.push(createElement('text', { textContent: child }))
+    }
+    return accu
+  }, [])
+
   return {
     type,
-    props,
-    children,
+    props: { ...props, children: childElements },
+    children: childElements,
   }
 }
